5c2033fa452cb66dd73c7851538f3c58710fa3ee
john russelle <kiddogreed@gmail.com>
2022-06-19 00:28:49 +0800

    add new shortcut

---
+++ Project.java
@@ -0,0 +1,12 @@
+public class Project {
+  
+}
+
+//data structure notes
+
+//algorithm - difference step/ways/computation in solving a problem
+
+//time complexity - how the actual number of  operation depend of the input size;
+
+//time complexity O(1) big O  // output still the same regadless of number of input
+//time complexity O(n) proportion to the size of input
